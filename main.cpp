8aee45aff9ce250dd357519eb5dcdf145fd2445b
Rafael Cavalcante <45821655+RafaelChavesPB@users.noreply.github.com>
2022-06-04 18:43:38 +0000

    Atualização do código

--- main.cpp
+++ main.cpp
@@ -1,52 +1,74 @@
 #include "bits/stdc++.h"
+#define A_ 1
+#define B_ 1
+#define C_ 1
 
 using namespace std;
+
+vector<vector<pair<int, int>>> lista_adj;
+vector<pair<int, int>> resultados;
+vector<int> visitados, caminho;
 random_device rd;
-vector<int> visitado; 
-vector<vector<pair<int,int>>> lista_adj;
-vector<pair<int,int>> resultados;
 
-void dfs(int v, int custo_ida, int custo_volta, bool ida){
-    visitado[v] = 1;
-    vector<pair<int, int>> melhores;
-    
-    for(auto it: lista_adj[v]){
-
-        if(visitado[it.second])
-            continue;
-        
-        melhores.push_back(it);
-    }
-    
-    if(!melhores.size())
-        return;
-
-    int escolhido = rd() % (melhores.size() < 3 ? melhores.size():3);
-    sort(melhores.begin(), melhores.end(), greater<pair<int,int>>());
-    custo_ida += melhores[escolhido].first*ida;
-    custo_volta += melhores[escolhido].first*(!ida);
-    v = melhores[escolhido].second;
-
-    if(ida)
-        resultados.push_back({custo_ida, custo_volta});
-
-    dfs(v, custo_ida, custo_volta, !ida);  
+int funcao_objetiva(int custo_ida, int custo_volta, int q_vertices) {
+	return A_ * custo_ida + B_ * custo_volta + C_ * q_vertices;
 }
 
-int main(){
-    int n, m;
-    cin >> n >> m;
+void dfs(int v, int custo_ida, int custo_volta, bool ida) {
+	vector<pair<int, int>> melhores;
+	visitados[v] = 1;
+	caminho.push_back(v);
+
+	for (auto it : lista_adj[v]) {
+		if (visitados[it.second])
+			continue;
+
+		melhores.push_back(it);
+	}
+
+	if (!melhores.size())
+		return;
+
+	int escolhido = rd() % (melhores.size() < 3 ? melhores.size() : 3);
+	sort(melhores.begin(), melhores.end(), greater<pair<int, int>>());
+	custo_ida += melhores[escolhido].first * ida;
+	custo_volta += melhores[escolhido].first * (!ida);
+	v = melhores[escolhido].second;
+
+	if (ida)
+		resultados.push_back({custo_ida, custo_volta});
+
+	dfs(v, custo_ida, custo_volta, !ida);
+}
+
+void grasp(int n) {
+	visitados.assign(n, 1);
+	resultados = vector<pair<int, int>>(1, {0, 0});
+	caminho.clear();
     
-    lista_adj.assign(n, vector<pair<int,int>>());
-    visitado.assign(n, 1);
+	dfs(0, 0, 0, true);
+	int idx = 0;
+    int melhor_resultado = 0;
+    
+    for(int i = 0; i < resultados.size(); i++)
+        if(funcao_objetiva(resultados[i].first, resultados[i].second, 2*i) > melhor_resultado)
+            idx = i;
+    
+    cout << resultados[idx].first << ' ' << resultados[idx].second << ' ' << 2*idx << endl;
+    for(int i = 0; i < 2*idx; i++)
+        cout << caminho[i] << ' ';
+    cout << endl;
+}
 
-    for(int i = 0; i < m; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        lista_adj[a].push_back({c,b});
-    }
+int main() {
+	int n, m;
+	cin >> n >> m;
 
+	lista_adj.assign(n, vector<pair<int, int>>());
 
-
-
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		lista_adj[a].push_back({c, b});
+	}
 }
